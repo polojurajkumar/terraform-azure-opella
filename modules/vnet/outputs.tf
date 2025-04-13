5f952498566503e84376f586d7b9507b6f5073a2
polojurajkumar <43132595+polojurajkumar@users.noreply.github.com>
2025-04-13 14:58:56 +0530

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -3,15 +3,15 @@
   #type        = string
 #}
 
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the virtual network"
-  type        = string
-}
+#variable "resource_group_name" {
+ # description = "The name of the resource group in which to create the virtual network"
+  #type        = string
+#}
 
-variable "location" {
-  description = "The location/region where the virtual network is created"
-  type        = string
-}
+#variable "location" {
+ # description = "The location/region where the virtual network is created"
+  #type        = string
+#}
 
 variable "address_space" {
   description = "The address space that is used by the virtual network"
