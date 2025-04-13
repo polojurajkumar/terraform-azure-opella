5a4dd694d4e5e693585503e58d6d17d0125e4e45
polojurajkumar <43132595+polojurajkumar@users.noreply.github.com>
2025-04-13 15:09:06 +0530

    outputs

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,80 +1,24 @@
-#variable "name" {
- # description = "The name of the virtual network"
-  #type        = string
-#}
-
-#variable "resource_group_name" {
- # description = "The name of the resource group in which to create the virtual network"
-  #type        = string
-#}
-
-#variable "location" {
- # description = "The location/region where the virtual network is created"
-  #type        = string
-#}
-
-variable "address_space" {
-  description = "The address space that is used by the virtual network"
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
+output "vnet_id" {
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
 }
 
-variable "subnets" {
-  description = "A map of subnet configurations"
-  type        = map(string)
-  default     = {
-    "subnet1" = "10.0.1.0/24"
-    "subnet2" = "10.0.2.0/24"
-  }
+output "vnet_name" {
+  description = "The name of the virtual network"
+  value       = azurerm_virtual_network.vnet.name
 }
 
-variable "tags" {
-  description = "A mapping of tags to assign to the resource"
-  type        = map(string)
-  default     = {}
+output "subnet_ids" {
+  description = "The IDs of the subnets"
+  value       = { for k, v in azurerm_subnet.subnets : k => v.id }
 }
 
-variable "create_nsg" {
-  description = "Whether to create a Network Security Group"
-  type        = bool
-  default     = true
+output "nsg_id" {
+  description = "The ID of the Network Security Group"
+  value       = var.create_nsg ? azurerm_network_security_group.nsg[0].id : null
 }
 
-variable "nsg_rules" {
-  description = "A list of Network Security Group rules"
-  type = list(object({
-    name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
-  }))
-  default = [
-    {
-      name                       = "AllowSSH"
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "22"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    },
-    {
-      name                       = "AllowHTTP"
-      priority                   = 110
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "80"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  ]
+output "nsg_name" {
+  description = "The name of the Network Security Group"
+  value       = var.create_nsg ? azurerm_network_security_group.nsg[0].name : null
 } 
