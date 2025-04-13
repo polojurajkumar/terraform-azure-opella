01119a6a12c881adbe976bca1ce2602975d1947e
polojurajkumar <43132595+polojurajkumar@users.noreply.github.com>
2025-04-13 14:55:16 +0530

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,7 +1,7 @@
-variable "name" {
-  description = "The name of the virtual network"
-  type        = string
-}
+#variable "name" {
+ # description = "The name of the virtual network"
+  #type        = string
+#}
 
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the virtual network"
